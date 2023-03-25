5ee5beccfc45b9c2d0701ae4c345cfb9cb0507bc
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-24 19:21:12 -0600

    Create DNode.java

---
+++ DNode.java
@@ -0,0 +1,18 @@
+package mylib.datastructures.nodes;
+// Hey Karam, you probably know about this already, but we have to build everything in maven, and I believe this is the correct package structure.
+// ^ temp comment.
+// Documentation outlined below:
+/**
+ * @authors Evan Barker & Karam Baroud
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class DNode {
+    DNode() {this.data = 0;}
+    public int data; // could be any other type of data, used to display functionality of the data structure.
+    public DNode next; // represents a pointer that points to the next node object in the linked list data structure.
+    public DNode back; // second pointer that points to previous node.
+    DNode(int data) { this.data = data; } // Constructor used to instantiate a new node object that contains data, and can be used to point to another node in the linked list.
+
+}
