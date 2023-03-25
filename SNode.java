62501973573a70d7eae741d355bfc03672461c65
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-24 19:18:28 -0600

    Update and rename DNode.java to SNode.java
    changed access modifiers to public

--- SNode.java
+++ SNode.java
@@ -4,13 +4,14 @@ package mylib.datastructures.nodes;
 // Documentation outlined below:
 /**
  * @authors Evan Barker & Karam Baroud
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  */
 
-public classSNode {
-    int data; // could be any other type of data, used to display functionality of the data structure.
-    SNode next; // represents a pointer that points to the next node object in the linked list data structure.
+public class SNode {
+    SNode() {this.data = 0;}
+    public int data; // could be any other type of data, used to display functionality of the data structure.
+    public SNode next; // represents a pointer that points to the next node object in the linked list data structure.
     SNode(int data) { this.data = data; } // Constructor used to instantiate a new node object that contains data, and can be used to point to another node in the linked list.
 
 }
