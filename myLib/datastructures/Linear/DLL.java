bc14e694e3f8b756729b822e4826ded4c5cfcbf3
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-25 19:09:24 -0600

    Create DLL.java

---
+++ myLib/datastructures/Linear/DLL.java
@@ -0,0 +1,201 @@
+
+package mylib.datastructures.linear;
+
+import mylib.datastructures.nodes.DNode;
+
+// Documentation outlined below:
+/**
+ * @authors Evan Barker & Karam Baroud
+ * @version 1.1
+ * @since 1.0
+ */
+
+public class DLL {
+    private DNode head;
+    private DNode tail;
+    private int size;
+
+    DLL() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    DLL(DNode headInput){
+        this.head = headInput;
+        this.tail = headInput;
+        DNode temporary = headInput;
+        while(temporary != null) {   // this is a way to update the size of the L.
+            this.size += 1;
+            temporary = temporary.next;
+        }
+
+    }
+
+    public void insertHead(DNode newNode) {
+        // One of two scenarios while trying to insert a new head.
+        if(this.head==null) {
+            this.head = newNode;
+            this.size += 1;
+        } else if (this.head != null) {
+            DNode temporary = newNode;
+            temporary.next = this.head;
+            this.head = temporary;
+            this.size += 1;
+        }
+    }
+
+    public void insertTail(DNode newNode) {
+        if(this.head==null) {
+            this.head = newNode;
+            this.size += 1;
+        } else if (this.head != null) {
+            DNode temporary = this.head;
+            while(temporary.next != null) {
+                temporary = temporary.next;
+            }
+            temporary.next = newNode;
+            this.size += 1;
+        }
+    }
+
+    public void insert(DNode newNode, int position) {
+        // One of three cases:
+        // 1) Position = 1, which means to insert at the head position
+        // 2) Position != 1 AND position != last position.
+        // 3) Position = last position of the linked list.
+
+        if(position==1) {
+            newNode.next = this.head;  // eliminated a temp pointer here.
+            this.head = newNode;
+        } else if (position != this.size) {
+            DNode current = this.head;
+            int currentNum = 1;
+            while(currentNum != position-1) {
+                current = current.next;
+                currentNum += 1;
+            }
+            newNode.next = current.next;
+            current = newNode;
+        }
+        else{
+            insertTail(newNode);  // I don't see why we can't use another method designed for this specifically...
+        }
+    }
+
+    public void sortedInsert(DNode node) {
+        // METHOD: for every position in the LL, we will check a 2-item array to see if
+        // the data is either smaller or larger than the data in the array; the data in the array
+        // contains the max and min ints SO FAR. Hence, I feel this is a fairly effecient way
+        // to check if something is out of order since we have O(2) v. O(n).
+        int[] maxMin = new int[2]; // storage for max and min ints
+        boolean didInsert = false;
+        int posCurr = 1;
+        DNode current = this.head;
+        while(!didInsert && current != null) {
+            boolean isStart = false;
+            if (maxMin[1] == 0 && maxMin[0] == 0) {
+                maxMin[1] = current.data;
+            }
+            if (maxMin[0] == 0 && !(maxMin[0] == 0)) {
+                if (current.data < maxMin[1]) {
+                    maxMin[0] = current.data;
+                } else {
+                    maxMin[0] = maxMin[1];
+                    maxMin[1] = current.data;
+                }
+                isStart = true; // Use this as a 'bool flag'
+            }
+            if (isStart) {
+                if (current.data < maxMin[1] && current.data > maxMin[0]) {
+                    sort(); // call sort to sort the LL
+                }
+            }
+            if ((current.data < node.data) && (current.next.data > node.data)) {
+                insert(node, posCurr);
+                didInsert = true; // breaks out of while loop if true.
+            }
+            current = current.next;
+            posCurr += 1;
+        }
+    }
+    public DNode Search(DNode node) {
+        DNode current = this.head;
+        while(current != null){
+            if(current == node){
+                return node;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    public void deleteHead() {
+        if(this.head==null){return;}
+        this.head = this.head.next;
+    }
+
+    public void deleteTail() {
+        DNode current = this.head;
+        while(current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+    }
+
+    public void delete(DNode node) {
+        if(this.head==node){deleteHead();}
+        DNode current = this.head;
+        DNode prev = null;
+        while(current != null){
+            prev = current;
+            current = current.next;
+            if(current==node){prev.next = current.next;}
+            return;
+        }
+    }
+
+    // decided to implement an insertion sort since that is what is asked for, although may come back to this and implement
+    // a merge sort if I ever get bored...
+    public void sort() {
+        // we can always find a better alg. later but this is what i got for now...
+        if(this.size == 0 || this.size == 1){
+            return;
+        }
+        else{
+            DNode starting = this.head;
+            DNode current = this.head;
+            while(current != null){
+                DNode before;
+                while(starting.next != null && starting.next.data <= current.data){
+                    before = starting;
+                    starting = starting.next;
+                }
+                DNode slotHolder = starting.next;
+                starting.next = current.next;
+                before = current;
+                current.next = slotHolder;
+                this.head = starting;
+
+                current = current.next;
+            }
+        }
+    }
+
+    public void clear() {
+        this.head = null;
+    }
+
+    public void print() {
+        DNode current = this.head;
+        System.out.print("List Information: \n");
+        System.out.printf("List length: %d", this.size);
+        System.out.print("Sorted status: ");               // still need to implement a method for this.
+        for(int i = 1; current != null; i++){
+            System.out.printf("Data in list item #%d: %d", i, current.data);
+            current = current.next;
+        }
+    }
+
+
+}
